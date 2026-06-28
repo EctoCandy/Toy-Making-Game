bff346c6691e83dd616014e808f2cbbbee437cfd
[EctoCandy] <[ectocandy@gmail.com]>
2026-06-27 22:52:20 -0500

    Added 5th music track

--- Scripts/music.gd
+++ Scripts/music.gd
@@ -4,6 +4,7 @@ const MUSIC_1 = preload("uid://c487py1l576k3")
 const MUSIC_2 = preload("uid://djj7shos1jp22")
 const MUSIC_3 = preload("uid://bd0ug4vi8spah")
 const MUSIC_4 = preload("uid://kmuk14kibi3r")
+const MUSIC_5 = preload("res://Assets/Music and SFX/melody alteration - inspo- snowdin town.mp3")
 const GAME_OVER_MUSIC = preload("uid://6tmbx7idvxa3")
 
 var last_track : int
@@ -13,7 +14,8 @@ var music_dict := {
 	1 : MUSIC_1,
 	2 : MUSIC_2,
 	3 : MUSIC_3,
-	4 : MUSIC_4
+	4 : MUSIC_4,
+	5 : MUSIC_5
 }
 
 func _ready() -> void:
@@ -27,9 +29,9 @@ func _ready() -> void:
 
 func _on_finished() -> void:
 	await get_tree().create_timer(3.0).timeout
-	var rand_track = randi_range(1, 4)
+	var rand_track = randi_range(1, 5)
 	while rand_track == last_track:
-		rand_track = randi_range(1, 4)
+		rand_track = randi_range(1, 5)
 	start_rand_track(rand_track)
 
 
